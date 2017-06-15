62d8f20a632b584b613d91edcfab2ae3e51d634d
valzie <skyla@users.noreply.github.com>
2017-06-15 15:37:28 -0400

    deployedversion2

--- js/app.js
+++ js/app.js
@@ -6,30 +6,27 @@ $(document).ready(function(){
   $('.startbutton').click(function(){
   myGame.shuffleTheDeck();//invokes the function startGame from simon.js
   });
-});
-  $('.nextcardbutton').click(function(){
 
+  $('.nextcardbutton').click(function(){
 
     seetheCard();
     //draws the card and populates it in one step below.
     $('#martiansdrawcard').html(myGame.drawTheMartianCard());
     $('#humansdrawcard').html(myGame.drawTheHumanCard());
-    this.determineTheScore();
-//     updatetheScore();
-//     fliptheCard();
-//       if(this.humanrandomDeck.length > 0){
-//         myGame.drawTheCard();
-//         populateTheCard();
-//         seetheCard();
-//         this.determineTheScore();
-//         updatetheScore();
-//         fliptheCard();
-// }
-// else{
-//        this.determineTheWinner();
+    // myGame.determineTheMartianScore();
+    // myGame.determineTheHumanScore();
+      updatetheScore();
+
+      setTimeout(function () {
+        fliptheCard();
+      }, 1000);
+      //I'm not saying this correctly. How do i get the length of an array?  Look it up.
+   if(this.humanrandomDeck.length <= 0){
+//        myGame.determineTheWinner();
           //updateTheWinner();
           //showTheWinner;
-// }
+        }
+});
 });
 //don't need this function since incorporated it to draw and pop card in one step in jquery above.
 // function populateCard(someCard) {
@@ -39,26 +36,54 @@ $(document).ready(function(){
 // }
 
 function seetheCard() {
+  // console.log('FRONT CARD');
   $(".martiancard").css('background-image', 'none');
   $(".martiancard").css({'background-color': 'white' });
   $(".humancard").css('background-image', 'none');
   $(".humancard").css({ 'background-color': 'white' });
 }
 function updatetheScore() {
-  $('.martianscore').html('this.martianScore');
-  $('.humanscore').html('this.humanScore');
+  //can't see the martianScore/humanScore because of the scope of the variable inside the function
+  // console.log(martianScore);
+  // console.log(humanScore);
+  $('.martianscore').html(myGame.determineTheMartianScore());
+  $('.humanscore').html(myGame.determineTheHumanScore());
+  // $('.martianscore').html(this.martianScore);
+  // $('.humanscore').html(this.humanScore);
 }
+
 function fliptheCard() {
-  $(".martiancard").css({ 'background-color': 'none' });
-  $(".martiancard").css('background-image', 'url("../images/Swampler by Katarzyna Zalecka.jpg")');
-  $(".humancard").css({ 'background-color': 'none' });
-  $(".humancard").css('background-image', 'url("../images/Swampler by Katarzyna Zalecka.jpg")');
+// console.log("I AM IN FLIPTHECARD (BACK)");
+// EMPTY the number
+    $('#martiansdrawcard').html(" ");
+    $('#humansdrawcard').html(" ");
+//     //flips the card back to the front of deck.
+    $(".martiancard").css({ 'background-color': 'none' });
+    $('.martiancard').css({'background-image' : 'url("./images/Swampler by Katarzyna Zalecka.jpg")'});
+    $(".humancard").css({ 'background-color': 'none' });
+    $('.humancard').css({'background-image' : 'url("./images/Swampler by Katarzyna Zalecka.jpg")'});
+
+
+
+
+
+  // $(".martiancard").css({'background-image' : url('../images/Swampler by Katarzyna Zalecka.jpg'});
+  // $('.martiancard').css({'background-image' : 'url("./images/Zabelin.png")'});
+  // $('.martiancard').css({'background-image' : 'url("../images/Swampler by Katarzyna Zalecka.jpg")'});
+// $('.buyButton_new').css('background-image','url(images/compra_mouseOver.png)');
+
+  //$(".humancard").css({ 'background-color': 'none' });
+  // $(".humancard").css('background-image', url('../images/Swampler by Katarzyna Zalecka.jpg'));
+  // $(".martiancard").css({ 'background-color': 'none' });
+  // $(".martiancard").css('background-image', 'url("../images/Swampler by Katarzyna Zalecka.jpg")');
+  // $(".humancard").css({ 'background-color': 'none' });
+  // $(".humancard").css('background-image', 'url("../images/Swampler by Katarzyna Zalecka.jpg")');
 }
-function updateTheWinner() {
-  $('#winnerloser').html('winner');//populates the humans Card
-  $('#winnerloser').html('winner');//populates the martians card
+//populates the text depending on whose score is higher.
+function updateTheWinner(){
+  $('#winnerloser').html(winner);
+  $('#winnerloser').html(winner);
 }
-function showTheWinner() {
-  $('#winnerloser').html('winner');//populates the humans Card
-  $('#winnerloser').html('winner');//populates the martians card
+function showTheWinner() {//shows the div.
+  $(".winnerloser").toggleClass("visible");
 }
--- js/war.js
+++ js/war.js
@@ -30,51 +30,84 @@ function Game () {
 
 
       this.drawTheHumanCard = function(){
-      console.log("We are in the drawTheCard");
+      console.log("We are in the drawTheHumanCard");
 
         // if(this.humanrandomDeck.length > 0){//>0 not working
         //   console.log("drawTheCard function");
-        this.humandrawnCard= this.humanrandomDeck.splice(0,1);
+        var cardArray = this.humanrandomDeck.splice(0,1);
+          // cardArray = [9];
+        this.humandrawnCard= cardArray[0];
         //this.determineTheScore();
       // };
       // else{
       //   this.determineTheWinner();
       // }
       //Successful Test
+
+      console.log(this.humandrawnCard);
       return "humandrawnCard: " + this.humandrawnCard;//0
        ///0
     };
     this.drawTheMartianCard = function(){
-    console.log("We are in the drawTheCard");
+    console.log("We are in the drawTheMartianCard");
 
       // if(this.humanrandomDeck.length > 0){//>0 not working
       //   console.log("drawTheCard function");
-      this.martiandrawnCard= this.martianrandomDeck.splice(0,1);
+      var cardArray= this.martianrandomDeck.splice(0,1);
+      this.martiandrawnCard= cardArray[0];
       //this.determineTheScore();
     // };
     // else{
     //   this.determineTheWinner();
     // }
     //Successful Test
-    return "martiandrawnCard: " + this.martiandrawnCard;//0
+    console.log(this.martiandrawnCard);
+    return "martiandrawnCard: " +this.martiandrawnCard;//0
      ///0
   };
 
-      this.determineTheScore = function(){
-      console.log("We are in the determineTheScore");
-        if(this.martiandrawnCard[0]>this.humandrawnCard[0]){
+      this.determineTheMartianScore = function(){
+        // var martianScore;
+
+        if(this.martiandrawnCard>this.humandrawnCard){
           this.martianScore+=1;
-          //this.drawTheCard();
-      }else if(this.humandrawnCard[0]>this.martiandrawnCard[0]){
-      this.humanScore+=1;
-       //this.drawTheCard();
-    }
+       }//else if(this.humandrawnCard[0]>this.martiandrawnCard[0]){
+    //   score=this.humanScore+=1;
+    //    //this.drawTheCard();
+    // }
     //Succeful Test
     // console.log("humandrawnCard: ",this.humandrawnCard);//0
     // console.log("martiandrawnCard: ", this.martiandrawnCard);//0
-    console.log("martianscore:", this.martianScore);
-    console.log("humanscore:", this.humanScore);
+    console.log("determineTheMartianScore ", {
+      martianScore: this.martianScore,
+      martiandrawnCard: this.martiandrawnCard,
+      humandrawnCard: this.humandrawnCard
+    });
+    //console.log("humanscore:", this.humanScore);
+    return this.martianScore;
         };
+
+        this.determineTheHumanScore = function(){
+
+        // console.log("We are in the determineTheHumanScore");
+          if(this.humandrawnCard>this.martiandrawnCard){
+            this.humanScore+=1;
+        }//else if(this.humandrawnCard[0]>this.martiandrawnCard[0]){
+        // humanScore=this.humanScore+=1;
+        //  //this.drawTheCard();
+        // }
+        //Succeful Test
+        // console.log("humandrawnCard: ",this.humandrawnCard);//0
+        // console.log("martiandrawnCard: ", this.martiandrawnCard);//0
+
+        console.log("determineTheHumanScore ", {
+          humanScore: this.martianScore,
+          martiandrawnCard: this.martiandrawnCard,
+          humandrawnCard: this.humandrawnCard
+        });
+        return this.humanScore;
+          };
+
 
         this.determineTheWinner= function(){
           var winner;
